b827a67467ff663121f668dc32d5ed47890199b2
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-12-30 18:46:41 -0400

    create view to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,7 +27,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.html { redirect_to categories_path, notice: "La nueva categoria fue creada con exito." }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
