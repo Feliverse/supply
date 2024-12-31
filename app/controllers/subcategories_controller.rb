97ac335c2c54117c7f7a7b800745164b3b5eb624
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-12-31 17:18:25 -0400

    Styling cards

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,72 +1,54 @@
 class SubcategoriesController < ApplicationController
-  before_action :set_subcategory, only: %i[ show edit update destroy ]
+  before_action :set_category
+  before_action :set_subcategory, only: [:show, :edit, :update, :destroy]
 
-  # GET /subcategories or /subcategories.json
   def index
-    @subcategories = Subcategory.all
+    @subcategories = @category.subcategories
   end
 
-  # GET /subcategories/1 or /subcategories/1.json
   def show
-    @subcategory = Subcategory.find(params[:id])
-    @marcas = @subcategory.marcas
   end
 
-  # GET /subcategories/new
   def new
-    @subcategory = Subcategory.new
+    @subcategory = @category.subcategories.build
   end
 
-  # GET /subcategories/1/edit
+  def create
+    @subcategory = @category.subcategories.build(subcategory_params)
+    if @subcategory.save
+      redirect_to category_subcategories_path(@category), notice: 'Subcategory was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
-  # POST /subcategories or /subcategories.json
-  def create
-    @subcategory = Subcategory.new(subcategory_params)
-
-    respond_to do |format|
-      if @subcategory.save
-        format.html { redirect_to @subcategory, notice: "Subcategory was successfully created." }
-        format.json { render :show, status: :created, location: @subcategory }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @subcategory.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /subcategories/1 or /subcategories/1.json
   def update
-    respond_to do |format|
-      if @subcategory.update(subcategory_params)
-        format.html { redirect_to @subcategory, notice: "Subcategory was successfully updated." }
-        format.json { render :show, status: :ok, location: @subcategory }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @subcategory.errors, status: :unprocessable_entity }
-      end
+    if @subcategory.update(subcategory_params)
+      redirect_to category_subcategories_path(@category), notice: 'Subcategory was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /subcategories/1 or /subcategories/1.json
   def destroy
     @subcategory.destroy
-
-    respond_to do |format|
-      format.html { redirect_to subcategories_path, status: :see_other, notice: "Subcategory was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to category_subcategories_path(@category), notice: 'Subcategory was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_subcategory
-      @subcategory = Subcategory.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def subcategory_params
-      params.require(:subcategory).permit(:name, :category_id)
-    end
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_subcategory
+    @subcategory = Subcategory.find(params[:id])
+  end
+
+  def subcategory_params
+    params.require(:subcategory).permit(:name)
+  end
 end
