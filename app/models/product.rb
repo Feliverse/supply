16331c770df27a08295568d89685a4df93ead7ef
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-01-07 19:48:12 -0400

    Styling Marcas

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :tech
+  belongs_to :category
+  belongs_to :subcategory
+  belongs_to :marca
+  belongs_to :tech, optional: true
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :category_id, presence: true, length: { maximum: 50 }
-  validates :subcategory_id, presence:, true, length: { maximum: 50 }
-  validates :marca_id, presence: true, length: { maximum: 50 }
-  validates :tech_id, presence: true, length: { maximum: 50 }
 end
