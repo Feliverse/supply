34e8d940f0de646f1ea9716807fd47e50666609a
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-01-15 21:26:05 -0400

    Almacenes Model and Controllers

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,9 @@
 class Cliente < ApplicationRecord
+  has_many :sales
+
+  validates :name, presence: true
+  validates :nit, presence: true, 
+    uniqueness: true, 
+    length: { minimum: 7, maximum: 15 }, 
+    numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :almacens
   resources :clientes
   resources :categories do 
     resources :subcategories
